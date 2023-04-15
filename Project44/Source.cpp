0fa1bf47ef5cc9733631983d2b6e8579ab2f6c5e
Abaidullah Asif <ubaidtech889@gmail.com>
2023-04-16 00:07:18 +0500

    Commit done

--- Project44/Source.cpp
+++ Project44/Source.cpp
@@ -1,25 +1,8 @@
 #include<SFML/Graphics.hpp>
-#include<iostream>
-using namespace std;
 
 
 
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode().getDesktopMode(), "ludo");
-
-	while(window.isOpen())
-	{
-
-		sf::Event event;
-
-
-		window.clear();
-		//window.draw();
-		window.display();
-
-
-
-	}
 }
