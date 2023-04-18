327da687d970b59b77c4724d246b4f30aea9f204
mahnoorkhan0939@gmail.com <mahnoorkhan0939@gmail.com>
2023-04-18 15:46:44 +0500

    movement done

--- Project44/Ludo.cpp
+++ Project44/Ludo.cpp
@@ -303,7 +303,710 @@ void Ludo::changeTurn()
 
 	//Turn = 0;
 }
+void Ludo::moveGoti(int& cur_x, int& cur_y)
+{
+	if ((cur_x >= 54 && cur_x < 146) && (cur_y == 290))
+	{
+		cur_x += 45;
+	}
+	else if ((cur_x == 146) && (cur_y == 290))
+	{
+		cur_x += 43;
+	}
+	else if ((cur_x == 189) && (cur_y == 290))
+	{
+		cur_x += 44;
+	}
+	else if ((cur_x == 233) && (cur_y == 290))
+	{
+		cur_x = 275;
+		cur_y = 246;
+	}
+	else if ((cur_x == 275) && (cur_y <= 246 && cur_y > 11))
+	{
+		cur_y -= 47;
+	}
+	else if ((cur_x >= 275 && cur_x < 363) && (cur_y == 11))
+	{
+		cur_x += 44;
+	}
+	else if ((cur_x == 363) && (cur_y >= 11 && cur_y < 246))
+	{
+		cur_y += 47;
+	}
+	else if ((cur_x == 363) && (cur_y == 246))
+	{
+		cur_x = 408;
+		cur_y = 292;
+	}
+	else if ((cur_x >= 408 && cur_x < 584) && (cur_y == 292))
+	{
+		cur_x += 44;
+	}
+	else if ((cur_x == 584) && (cur_y == 292))
+	{
+		cur_x = 629;
+		cur_y = 292;
+	}
+	else if ((cur_x == 629) && (cur_y == 292))
+	{
+		cur_x = 672;
+		cur_y = 246;
+	}
+	else if ((cur_x == 672) && (cur_y <= 246 && cur_y > 11))
+	{
+		cur_y -= 47;
+	}
+	else if ((cur_x == 672) && (cur_y == 11))
+	{
+		cur_x = 716;
+		cur_y = 11;
+	}
+	else if ((cur_x == 716) && (cur_y == 11))
+	{
+		cur_x = 760;
+		cur_y = 11;
+	}
+	else if ((cur_x == 760) && (cur_y < 246 && cur_y >= 11))
+	{
+		cur_y += 47;
+	}
+	else if ((cur_x == 760) && (cur_y == 246))
+	{
+		cur_x = 804;
+		cur_y = 292;
+	}
+	else if ((cur_x >= 804 && cur_x < 1024) && (cur_y == 292))
+	{
+		cur_x += 44;
+	}
+	else if ((cur_x == 1024) && (cur_y < 386 && cur_y >= 292))
+	{
+		cur_y += 47;
+	}
+	else if ((cur_x > 804 && cur_x <= 1024) && (cur_y == 386))
+	{
+		cur_x -= 44;
+	}
+	else if ((cur_x == 804) && (cur_y == 386))
+	{
+		cur_x = 760;
+		cur_y = 432;
+	}
+	else if ((cur_x == 760) && (cur_y < 620 && cur_y >= 432))
+	{
+		cur_y += 47;
+	}
+	else if ((cur_x == 760) && (cur_y == 620))
+	{
+		cur_x = 760;
+		cur_y = 662;
+	}
+	else if ((cur_x <= 760 && cur_x > 672) && (cur_y == 662))
+	{
+		cur_x -= 44;
+	}
+	else if ((cur_x == 672) && (cur_y <= 662 && cur_y > 572))
+	{
+		cur_y -= 45;
+	}
+	else if ((cur_x == 672) && (cur_y <= 572 && cur_y > 431))
+	{
+		cur_y -= 47;
+	}
+	else if ((cur_x == 672) && (cur_y == 431))
+	{
+		cur_x = 629;
+		cur_y = 386;
+	}
+	else if ((cur_x == 629) && (cur_y == 386))
+	{
+		cur_x = 584;
+		cur_y = 386;
+	}
+	else if ((cur_x <= 584 && cur_x > 408) && (cur_y == 386))
+	{
+		cur_x -= 44;
+	}
+	else if ((cur_x == 408) && (cur_y == 386))
+	{
+		cur_x = 363;
+		cur_y = 432;
+	}
+	else if ((cur_x == 363) && (cur_y >= 432 && cur_y < 620))
+	{
+		cur_y += 47;
+	}
+	else if ((cur_x == 363) && (cur_y == 620))
+	{
+		cur_x = 363;
+		cur_y = 662;
+	}
+	else if ((cur_x > 275 && cur_x <= 363) && (cur_y == 662))
+	{
+		cur_x -= 44;
+	}
+	else if ((cur_x == 275) && (cur_y == 662))
+	{
+		cur_x = 275;
+		cur_y = 617;
+	}
+	else if ((cur_x == 275) && (cur_y == 617))
+	{
+		cur_x = 275;
+		cur_y = 572;
+	}
+	else if ((cur_x == 275) && (cur_y <= 572 && cur_y > 431))
+	{
+		cur_y -= 47;
+	}
+	else if ((cur_x == 275) && (cur_y == 431))
+	{
+		cur_x = 231;
+		cur_y = 386;
+	}
+	else if ((cur_x == 231) && (cur_y == 386))
+	{
+		cur_x = 187;
+		cur_y = 386;
+	}
+	else if ((cur_x == 187) && (cur_y == 386))
+	{
+		cur_x = 144;
+		cur_y = 386;
+	}
+	else if ((cur_x == 144) && (cur_y == 386))
+	{
+		cur_x = 99;
+		cur_y = 386;
+	}
+	else if ((cur_x == 99) && (cur_y == 386))
+	{
+		cur_x = 54;
+		cur_y = 386;
+	}
+	else if ((cur_x == 54) && (cur_y == 386))
+	{
+		cur_x = 11;
+		cur_y = 386;
+	}
+	else if ((cur_x == 11) && (cur_y == 386))
+	{
+		cur_x = 11;
+		cur_y = 339;
+	}
+	else if ((cur_x == 11) && (cur_y == 339))
+	{
+		cur_x = 11;
+		cur_y = 292;
+	}
+	else if ((cur_x == 11) && (cur_y == 292))
+	{
+		cur_x = 54;
+		cur_y = 290;
+	}
+}
+bool Ludo::movetowardsHome(bool move, int Turn, int& cur_x, int& cur_y, int goti_no)
+{
+	if ((cur_x >= 11 && cur_x <= 233) && (cur_y == 339) && Turn == 0)
+	{
+		if (move == true)
+		{
+			if ((cur_x >= 11 && cur_x < 146) && (cur_y == 339))
+			{
+				cur_x += 45;
+			}
+			else if ((cur_x == 146) && (cur_y == 339))
+			{
+				cur_x += 43;
+			}
+			else if ((cur_x == 189) && (cur_y == 339))
+			{
+				cur_x += 44;
+			}
+			else if (cur_x == 233 && cur_y == 339)
+			{
+				if (goti_no == 1)
+				{
+					cur_x = 269;
+					cur_y = 337;
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 337);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 2)
+				{
+					cur_x = 269;
+					cur_y = 309;
 
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 309);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 3)
+				{
+					cur_x = 269;
+					cur_y = 365;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 365);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 4)
+				{
+					cur_x = 296;
+					cur_y = 337;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(296, 337);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+			}
+		}
+		return true;
+	}
+	else if ((cur_y >= 11 && cur_y <= 246) && (cur_x == 319) && Turn == 1)
+	{
+		if (move == true)
+		{
+			if ((cur_y >= 11 && cur_y < 246) && (cur_x == 319))
+			{
+				cur_y += 47;
+			}
+			else if (cur_y == 246 && cur_x == 319)
+			{
+
+				if (goti_no == 1)
+				{
+					cur_x = 319;
+					cur_y = 313;
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 337);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 2)
+				{
+					cur_x = 319;
+					cur_y = 284;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 309);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 3)
+				{
+					cur_x = 290;
+					cur_y = 284;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 365);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 4)
+				{
+					cur_x = 348;
+					cur_y = 284;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(296, 337);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+			}
+		}
+		return true;
+	}
+	else if ((cur_y >= 11 && cur_y <= 246) && (cur_x == 716) && Turn == 2)
+	{
+		if (move == true)
+		{
+			if ((cur_y >= 11 && cur_y < 246) && (cur_x == 716))
+			{
+				cur_y += 47;
+			}
+			else if (cur_y == 246 && cur_x == 716)
+			{
+
+				if (goti_no == 1)
+				{
+					cur_x = 716;
+					cur_y = 313;
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 337);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 2)
+				{
+					cur_x = 716;
+					cur_y = 284;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 309);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 3)
+				{
+					cur_x = 687;
+					cur_y = 284;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 365);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 4)
+				{
+					cur_x = 745;
+					cur_y = 284;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(296, 337);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+			}
+
+		}
+		return true;
+	}
+	else if ((cur_x >= 804 && cur_x <= 1025) && (cur_y == 338) && Turn == 3)
+	{
+		if (move == true)
+		{
+			if ((cur_x > 804 && cur_x <= 1025) && (cur_y == 338))
+			{
+				cur_x -= 44;
+			}
+			else if (cur_x == 804 && cur_y == 338)
+			{
+				if (goti_no == 1)
+				{
+					cur_x = 739;
+					cur_y = 337;
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 337);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 2)
+				{
+					cur_x = 766;
+					cur_y = 337;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 309);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 3)
+				{
+					cur_x = 766;
+					cur_y = 308;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 365);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 4)
+				{
+					cur_x = 766;
+					cur_y = 366;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(296, 337);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+			}
+		}
+		return true;
+	}
+	else if ((cur_y >= 431 && cur_y <= 662) && (cur_x == 716) && Turn == 4)
+	{
+		if (move == true)
+		{
+			if ((cur_x == 716) && (cur_y <= 662 && cur_y > 572))
+			{
+				cur_y -= 45;
+			}
+			else if ((cur_x == 716) && (cur_y <= 572 && cur_y >= 431))
+			{
+				cur_y -= 47;
+			}
+			else if (cur_x == 716 && cur_y == 431)
+			{
+				if (goti_no == 1)
+				{
+					cur_x = 716;
+					cur_y = 363;
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 337);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 2)
+				{
+					cur_x = 716;
+					cur_y = 391;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 309);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 3)
+				{
+					cur_x = 688;
+					cur_y = 391;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 365);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 4)
+				{
+					cur_x = 744;
+					cur_y = 391;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(296, 337);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+			}
+		}
+		return true;
+	}
+	else if ((cur_y >= 431 && cur_y <= 662) && (cur_x == 319) && Turn == 5)
+	{
+		if (move == true)
+		{
+			if ((cur_x == 319) && (cur_y <= 662 && cur_y > 572))
+			{
+				cur_y -= 45;
+			}
+			else if ((cur_x == 319) && (cur_y <= 572 && cur_y >= 431))
+			{
+				cur_y -= 47;
+			}
+			else if (cur_x == 716 && cur_y == 431)
+			{
+				if (goti_no == 1)
+				{
+					cur_x = 347;
+					cur_y = 391;
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 337);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 2)
+				{
+					cur_x = 291;
+					cur_y = 391;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 309);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 3)
+				{
+					cur_x = 319;
+					cur_y = 391;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(269, 365);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+				else if (goti_no == 4)
+				{
+					cur_x = 319;
+					cur_y = 360;
+
+					//Players[Turn].getPiece(goti_no)->setPosition(296, 337);
+					Players[Turn].getPiece(goti_no)->setisPassed(true);
+					Players[Turn].piecePassed();
+				}
+			}
+		}
+		return true;
+	}
+
+	return false;
+} //
+bool Ludo::canGoHome(int Turn, int kitnay_ay, int goti_no)
+{
+
+	int tempx = Players[Turn].getPiece(goti_no)->getPosition_X();
+	int tempy = Players[Turn].getPiece(goti_no)->getPosition_Y();
+	bool ja_skta = false;
+
+	for (int i = 0; i < kitnay_ay; i++)
+	{
+		if (movetowardsHome(false, Turn, tempx, tempy, goti_no) == false)
+		{
+			moveGoti(tempx, tempy);
+		}
+		if (movetowardsHome(false, Turn, tempx, tempy, goti_no) == true)
+		{
+			movetowardsHome(true, Turn, tempx, tempy, goti_no);
+			ja_skta = true;
+		}
+	}
+
+	if (ja_skta == true)
+	{
+		return true;
+	}
+	return false;
+
+}
+bool Ludo::checkSavePoints(int cur_x, int cur_y)
+{
+	// stars
+
+	if ((cur_x == 275) && (cur_y == 105))
+	{
+		return true;
+	}
+	else if ((cur_x == 672) && (cur_y == 105))
+	{
+		return true;
+	}
+	else if ((cur_x == 936) && (cur_y == 292))
+	{
+		return true;
+	}
+	else if ((cur_x == 760) && (cur_y == 573))
+	{
+		return true;
+	}
+	else if ((cur_x == 363) && (cur_y == 573))
+	{
+		return true;
+	}
+	else if ((cur_x == 99) && (cur_y == 386))
+	{
+		return true;
+	}
+
+	// home
+
+	else if ((cur_x == 54) && (cur_y == 290))
+	{
+		return true;
+	}
+	else if ((cur_x == 363) && (cur_y == 58))
+	{
+		return true;
+	}
+	else if ((cur_x == 760) && (cur_y == 58))
+	{
+		return true;
+	}
+	else if ((cur_x == 980) && (cur_y == 386))
+	{
+		return true;
+	}
+	else if ((cur_x == 672) && (cur_y == 617))
+	{
+		return true;
+	}
+	else if ((cur_x == 275) && (cur_y == 617))
+	{
+		return true;
+	}
+
+
+	return false;
+}
+void Ludo::checkKill(int Turn, int goti_no, bool& now_move)
+{
+	int kill_goti_no = 0;
+	int kill_playerno = 0;
+	bool kill = false;
+
+
+	for (int j = 0; j < 6; j++)
+	{
+		if (j != Turn)
+		{
+			for (int k = 0; k < 4; k++)
+			{
+				if ((Players[Turn].getPiece(goti_no)->getPosition_X() == Players[j].getPiece(k)->getPosition_X()) && (Players[Turn].getPiece(goti_no)->getPosition_Y() == Players[j].getPiece(k)->getPosition_Y()))
+				{
+					kill_goti_no = k;
+					kill_playerno = j;
+					kill = true;
+				}
+			}
+		}
+	}
+
+
+	if (kill == true)
+	{
+		Players[kill_playerno].getPiece(kill_goti_no)->setPosition(Players[kill_playerno].Pieces_cord[kill_goti_no][0], Players[kill_playerno].Pieces_cord[kill_goti_no][1]);
+		Players[kill_playerno].getPiece(kill_goti_no)->setatHome(true);
+		Players[kill_playerno].pieceHomeIn();
+		now_move = false;
+	}
+
+
+
+}
+int Ludo::boxestoCenter(int Turn, int goti_no)
+{
+	int tempx = Players[Turn].getPiece(goti_no)->getPosition_X();
+	int tempy = Players[Turn].getPiece(goti_no)->getPosition_Y();
+	int count = 0;
+	if (Turn == 0)
+	{
+		while (tempx <= 262)
+		{
+			tempx += 42;
+			count++;
+		}
+		return count;
+	}
+	else if (Turn == 1)
+	{
+		while (tempy <= 252)
+		{
+			tempy += 42;
+			count++;
+		}
+		return count;
+	}
+	else if (Turn == 2)
+	{
+		while (tempy <= 252)
+		{
+			tempy += 42;
+			count++;
+		}
+		return count;
+	}
+	else if (Turn == 3)
+	{
+		while (tempx >= 790)
+		{
+			tempx -= 42;
+			count++;
+		}
+		return count;
+	}
+
+	else if (Turn == 4)
+	{
+		while (tempy >= 421)
+		{
+			tempy -= 42;
+			count++;
+		}
+		return count;
+	}
+	else if (Turn == 5)
+	{
+		while (tempy >= 421)
+		{
+			tempy -= 42;
+			count++;
+		}
+		return count;
+	}
+
+}
 
 void Ludo::Play()
 {
@@ -377,7 +1080,6 @@ void Ludo::Play()
 
 					if (now_move == false)
 					{
-
 						//cout << roll_value;
 						//d.rollDice();
 						//cout << Turn << " ";
@@ -415,9 +1117,118 @@ void Ludo::Play()
 
 				//cout << position.x << "," << position.y << " ";
 				//cout << diceValue.size() << endl;;
+				if (now_move == true)
+				{
+					int goti_no = -1;
+					if (Players[Turn].checkGoti(position.x, position.y, goti_no) == true)
+					{
+
+						if (Players[Turn].getPiece(goti_no)->getatHome() == true && find(diceValue.begin(), diceValue.end(), 6) != diceValue.end())
+						{
+							int cordx = Players[Turn].Home_cord[0];
+							int cordy = Players[Turn].Home_cord[1];
+							Players[Turn].pieceHomeOut();
+							Players[Turn].getPiece(goti_no)->setatHome(false);
+							//Players[Turn].pieceHomeOut();
+
+							std::vector<int>::iterator position = std::find(diceValue.begin(), diceValue.end(), 6);
+							if (position != diceValue.end())
+							{
+								diceValue.erase(position);
+							}
+							Players[Turn].getPiece(goti_no)->setPosition(cordx, cordy);
+						}
+						else if (Players[Turn].getPiece(goti_no)->getatHome() == false)
+						{
+							if (kitnay_ay > 0)
+							{
+								if (canGoHome(Turn, kitnay_ay, goti_no) == false)
+								{
+									for (int i = 0; i < kitnay_ay; i++)
+									{
+										int tempx = Players[Turn].getPiece(goti_no)->getPosition_X();
+										int tempy = Players[Turn].getPiece(goti_no)->getPosition_Y();
+										moveGoti(tempx, tempy);
+										Players[Turn].getPiece(goti_no)->setPosition(tempx, tempy);
+									}
+									if (checkSavePoints(Players[Turn].getPiece(goti_no)->getPosition_X(), Players[Turn].getPiece(goti_no)->getPosition_Y()) == false)
+									{
+										checkKill(Turn, goti_no, now_move);
+									}
+									std::vector<int>::iterator position = std::find(diceValue.begin(), diceValue.end(), kitnay_ay);
+									if (position != diceValue.end()) {
+										diceValue.erase(position);
+									}
+									kitnay_ay = 0;
+								}
+								else
+								{
+									int itny_rah_gay = 0;
+									int tempx = Players[Turn].getPiece(goti_no)->getPosition_X();
+									int tempy = Players[Turn].getPiece(goti_no)->getPosition_Y();
+									//int count = boxestoCenter(Turn, goti_no);
+									for (int i = 0; i < kitnay_ay; i++)
+									{
+										itny_rah_gay = kitnay_ay - (i + 1);
+										if (movetowardsHome(false, Turn, tempx, tempy, goti_no) == true)
+										{
+											if (Players[Turn].getPiece(goti_no)->getPosition_X() >= Players[Turn].End_cord[0] && Players[Turn].getPiece(goti_no)->getPosition_Y() >= Players[Turn].End_cord[1])
+											{
+												if (boxestoCenter(Turn, goti_no) >= kitnay_ay)
+												{
+													movetowardsHome(true, Turn, tempx, tempy, goti_no);
+													move_hogya = true;
+												}
+											}
+										}
+										else
+										{
+											/*int tempx = Players[Turn].getPiece(goti_no)->getPosition_X();
+											int tempy = Players[Turn].getPiece(goti_no)->getPosition_Y();*/
+											moveGoti(tempx, tempy);
+											//Players[Turn].getPiece(goti_no)->setPosition(tempx, tempy);
+											if (checkSavePoints(Players[Turn].getPiece(goti_no)->getPosition_X(), Players[Turn].getPiece(goti_no)->getPosition_Y()) == false)
+											{
+												checkKill(Turn, goti_no, now_move);
+											}
+											Players[Turn].getPiece(goti_no)->setPosition(tempx, tempy);
+										}
 
 
+
+
+									}
+									if (move_hogya == true)
+									{
+										Players[Turn].getPiece(goti_no)->setPosition(tempx, tempy);
+										std::vector<int>::iterator position = std::find(diceValue.begin(), diceValue.end(), kitnay_ay);
+										if (position != diceValue.end()) {
+											diceValue.erase(position);
+										}
+										kitnay_ay = 0;
+									}
+								}
+							}
+						}
+
+
+					}
+					else
+					{
+
+					}
+					if (diceValue.size() == 0 && now_move == true)
+					{
+						//cout << Turn << " ";
+						six_count = 0;
+						changeTurn();
+						now_move = false;
+						move_hogya = false;
+					}
+
+				}
 				break;
+
 			}
 			case sf::Event::KeyPressed:
 			{
